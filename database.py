bc3f92c5c0ba1d7fc0673b7ce333bf85d70194b0
Santiago Collazo <santiago.collazo7@gmail.com>
2022-12-07 20:50:16 -0300

    Actualice database.py

--- database.py
+++ database.py
@@ -1,26 +1,76 @@
 import sqlite3
 
 class Database:
-    def add_highscore (nombre,puntuacion):
+    """
+    Base de datos para la tabla de puntuaciones.
+    """
+    
+    def add_highscore (nombre:str,score:int) -> None:
+        """
+        Agrega el nombre y puntuación de un nuevo jugador a la base de datos.
+        
+        No retorna nada.
+        
+        ----------
+        nombre : str
+            nombre del jugador
+        puntuacion : int
+            puntuación del jugador
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
-            database.execute("INSERT into highscore (nombre,puntuacion) values (?,?)", (nombre,puntuacion))
+            database.execute("INSERT into highscore (nombre,puntuacion) values (?,?)", (nombre,score))
             database.commit()
             
-    def update_highscore(nombre,score):
+    def update_highscore(nombre:str,score:int) -> None:
+        """
+        Actualiza la puntuación de un jugador ya registrado en la base de datos.
+        
+        No retorna nada.
+        
+        ----------
+        nombre : str
+            nombre del jugador registrado
+        puntuacion : int
+            puntuación del jugador
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             update = database.execute("UPDATE highscore SET puntuacion = '{0}' WHERE nombre=?".format(score),(nombre,))
             players = update.fetchall()
             for player in players:
                 print(player)
             
-    def delete_highscore(nombre):
+    def delete_highscore(nombre:str) -> None:
+        """
+        Borra la entrada en la base de datos correspondiente al nombre recibido.
+        
+        No retorna nada.
+        
+        ----------
+        nombre : str
+            nombre del jugador a borrar
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             update = database.execute("DELETE FROM highscore WHERE nombre=?",(nombre,))
             players = update.fetchall()
             for player in players:
                 print(player)
     
-    def check_registered_highscore (nombre):
+    def check_registered_highscore (nombre:str) -> bool:
+        """
+        Comprueba si el nombre recibido ya existe en la base de datos.
+        
+        Si es el caso, retorna True.
+        
+        Si no es el caso, retorna False.
+        
+        ----------
+        nombre : str
+            nombre del jugador
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             display = database.execute("SELECT * FROM highscore")
             for player in display:
@@ -29,7 +79,21 @@ class Database:
                 else:
                     return False
                 
-    def compare_highscore (nombre,score):
+    def compare_highscore (nombre:str,score:int) -> bool:
+        """
+        Comprueba si la la puntuación recibida es mayor que la puntuación registrada del jugador indicado.
+        
+        Si es el caso, retorna True.
+        
+        Si no es el caso, retorna False.
+        
+        ----------
+        nombre : str
+            nombre del jugador registrado
+        puntuacion : int
+            nueva puntuación del jugador
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             display = database.execute("SELECT * FROM highscore WHERE nombre=?",(nombre,))
             for player in display:
@@ -38,16 +102,43 @@ class Database:
                 else:
                     return False
             
-    def display_highscore (nombre):
+    def display_highscore (nombre:str):
+        """
+        Busca en la base de datos la entrada correspondiente al nombre recibido.
+        
+        Retorna los datos de la entrada.
+        
+        ----------
+        nombre : str
+            nombre del jugador a buscar
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             display = database.execute("SELECT * FROM highscore WHERE nombre=?",(nombre,))
             for player in display:
                 return player
 
     def display_all_highscore ():
+        """
+        Comprueba si la la puntuación recibida es mayor que la puntuación registrada del jugador indicado.
+        
+        Si es el caso, retorna True.
+        
+        Si no es el caso, retorna False.
+        
+        ----------
+        nombre : str
+            nombre del jugador registrado
+        puntuacion : int
+            nueva puntuación del jugador
+        """
+        
         with sqlite3.connect("db_highscore.db") as database:
             highscore_list = []
             display = database.execute("SELECT ID,nombre,puntuacion FROM highscore ORDER BY puntuacion DESC LIMIT 5;")
             for player in display:
                 highscore_list.append(player)
             return highscore_list
+        
+        
+print(type(Database.display_highscore("AAAA")))
