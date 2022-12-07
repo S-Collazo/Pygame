100bd821f7d6411b5741fb1d903401507c399caf
S-Collazo <108555088+S-Collazo@users.noreply.github.com>
2022-12-07 18:13:39 -0300

    Update damage_control.py

--- damage_control.py
+++ damage_control.py
@@ -2,7 +2,29 @@ import pygame
 from constants import *
 
 class Damage_Control:
-    def __init__(self, lista_personajes, lista_enemigos, lista_balas, lista_trampas,sounds):
+    """
+    Controlador de daños entre elementos del nivel.
+    """
+    
+    def __init__(self, lista_personajes:list, lista_enemigos:list, lista_balas:list, lista_trampas:list,sounds) -> None:
+        """
+        Carga las listas de elementos activos y el controlador de sonidos a las variables correspondientes.
+        
+        No retorna nada.
+        
+        ----------
+        lista_personajes : list
+            lista de los jugadores activos en el nivel
+        lista_enemigos : list
+            lista de los enemigos activos en el nivel
+        lista_balas : list
+            lista de las balas activas en el nivel
+        lista_trampas : list
+            lista de las trampas activas en el nivel
+        sounds
+            objeto controlador de sonidos
+        """
+        
         self.lista_personajes = lista_personajes
         self.lista_enemigos = lista_enemigos
         self.lista_balas = lista_balas
@@ -10,12 +32,31 @@ class Damage_Control:
 
         self.sounds = sounds
 
-    def damage(self, lista_atacante, lista_atacado):
+    def damage(self, lista_atacante:list, lista_atacado:list) -> None:
+        """
+        Controla las colisiones entre distintos elementos de dos listas para determinar si uno sufre daños. 
+        También reproduce los sonidos de ataque/ bloqueo.
+        
+        No funciona si el elemento atacado no tiene el estado "vivo" o si el elemento atacante no está realizando la acción "atacar"/"disparar".
+        
+        No retorna nada.
+        
+        ----------
+        lista_atacante : list
+            lista de los elementos que causan el daño
+        lista_atacado : list
+            lista de los elementos que sufren el daño
+            
+        ----------
+        DEBUG: En caso de daños, hace un print indicando el nombre del atacante y el atacado.
+        """
+        
         for atacante in lista_atacante:
             for atacado in lista_atacado:
                 if (atacado.is_alive):
                     if not (atacado.is_dying or atacado.is_hurt) and (atacante.is_attack or atacante.is_shoot) and not (atacado.asset_name == atacante.asset_name): 
                         if((atacante.rect_body_collition.colliderect(atacado.rect_collition))):
+                            # Si el atacado está realizando la acción "bloquear", no sufre daños pero se reproduce el sonido correspondiente:
                             if(atacado.is_block):
                                 self.sounds.sound_effect(atacado.block_sound)
                             else:
@@ -25,11 +66,14 @@ class Damage_Control:
                                 if(DEBUG):
                                     print("{0} hit {1}".format(atacante.asset_name,atacado.asset_name))
                                 
+                                # Desplaza al atacado lejos del atacante:
                                 if(atacante.rect.x <= atacado.rect.x):
                                     atacado.add_x(25)
                                 else:
                                     atacado.add_x(-25)    
                                 
+                                # Si el ataque agotaría los puntos de vida del atacado, se activa la animación y estado de muerte.
+                                # Si no, los de herida:
                                 if(atacado.hitpoints - atacante.attack_power <= 0):
                                     atacado.is_dying = True
                                     atacado.death()
@@ -37,11 +81,13 @@ class Damage_Control:
                                     atacado.is_hurt = True
                                     atacado.hurt()
                             
-    def update(self):
+    def update(self) -> None:
+        """
+        Actualiza las listas de elementos activos en el nivel y ejecuta el método de control de daños entre cada una de ellas.
+        """
+        
         self.lista_entidades = self.lista_personajes + self.lista_enemigos
         self.damage(self.lista_personajes, self.lista_enemigos)
         self.damage(self.lista_enemigos, self.lista_personajes)
         self.damage(self.lista_trampas, self.lista_entidades)
         self.damage(self.lista_balas, self.lista_entidades)
-        
-        
