e6b6a22d04e9e5d59bff0b90a5062c85b41f70b5
S-Collazo <108555088+S-Collazo@users.noreply.github.com>
2022-12-07 16:20:30 -0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -3,20 +3,56 @@ from constants import *
 from auxiliar import Auxiliar
 
 class Bullet:
-    def __init__ (self,asset,x,y,move_rate_ms,frame_rate_ms,move=100,direction_inicial=DIRECTION_R,p_scale=1,interval_bullet=FPS*2,distance=ANCHO_VENTANA,type=0):
+    """
+    Proyectiles disparados por los personajes del juego.
+    """
+    
+    def __init__ (self,asset:dict,x:int,y:int,move_rate_ms:int,frame_rate_ms:int,move:int=100,direction_inicial:int=DIRECTION_R,p_scale:int=1,interval_bullet:int=FPS*2,distance:int=ANCHO_VENTANA) -> None:
+        """
+        Extrae información del diccionario para determinar nombre del personaje que dispara, nombre de la bala, sprite a animar, 
+        puntos de ataque y sonido de impacto. Usa los parámetros pasados para determinar distancia y velocidad de movimiento.
+        
+        También genera un rectángulo de colisión con las mismas dimensiones que el sprite.
+        
+        No retorna nada.
+        
+        ----------
+        asset : dict
+            diccionario con toda la información del personaje que dispara la bala (contiene también la información de la bala)
+        x : int
+            coordenada X inicial de la bala
+        y : int
+            coordenada y inicial de la bala   
+        move_rate_ms : int
+            velocidad de desplazamiento
+        frame_rate_ms : int
+            velocidad de las animaciones  
+        move : int
+            distancia a recorrer por cada frame. Por defecto, 100
+        direction_inicial : int
+            dirección en la que se dispara la bala. Por defecto, derecha
+        p_scale : int
+            escala del sprite. Por defecto, 1
+        interval_bullet : int
+            intervalo de tiempo para actualizar la posición de la bala. Por defecto, el doble de los FPS del nivel
+        distance : int
+            distancia máxima que puede recorrer la bala antes de desaparecer. Por defecto, el ancho de la ventana
+        """
+        
         self.p_scale = p_scale * GLOBAL_SCALE
         self.asset = asset
         self.asset_name = asset["name"]
         self.bullet_asset = asset["bullet"]
         self.bullet_asset_name = self.bullet_asset["name"]
         
+        # Determina dirección de la bala y animación correspondiente:
         self.direction = direction_inicial
-        
         if(self.direction == DIRECTION_L):
             self.image_list= Auxiliar.getSurfaceFromSeparateFiles(PATH_RECURSOS + self.bullet_asset["path"] + "_{:03d}.png",self.bullet_asset["quantity"],flip=False,step=0,scale=self.p_scale,w=100,h=100)
         else:
             self.image_list= Auxiliar.getSurfaceFromSeparateFiles(PATH_RECURSOS + self.bullet_asset["path"] + "_{:03d}.png",self.bullet_asset["quantity"],flip=True,step=0,scale=self.p_scale,w=100,h=100)
         
+        # Variables de animación y rectángulo del sprite de la bala:
         self.frame = 0
         self.animation = self.image_list
         self.image = self.animation[self.frame]
@@ -24,11 +60,17 @@ class Bullet:
         self.rect.x = x
         self.rect.y = y
         
+        # Puntos de ataque, sonido de impacto y estados de la bala (disparada y atacando):
         self.attack_power = self.bullet_asset["attack_power"]
         self.attack_sound = self.bullet_asset["sound_effect"]
+        self.is_shoot = True
+        self.is_attack = False
         
+        # Estado de colisiones de la bala y rectángulo de colisión:
+        self.collition_enabled = True
         self.rect_body_collition = pygame.Rect(self.rect)
         
+        # Variavles de animaciones y desplazamiento:
         self.delta_x = move
         self.frame_rate_ms = frame_rate_ms
         self.move_rate_ms = move_rate_ms * FPS
@@ -37,13 +79,29 @@ class Bullet:
         self.tiempo_transcurrido_move = 0
         self.tiempo_transcurrido_anim = 0
         self.interval_bullet = interval_bullet
+            
+    def shooting (self,lista_personajes:list,lista_enemigos:list,lista_plataformas:list,lista_trampas:list,lista_balas:list) -> None:
+        """
+        Controla si la bala colisiona con alguno de los elementos activos del nivel (ignorando la propia bala).
+        De ser el caso, la desactiva.
         
-        self.is_shoot = True
-        self.is_attack = False
-        self.collition_enabled = True
-    
-    def shooting (self,lista_personajes,lista_enemigos,lista_plataformas,lista_trampas,lista_balas):
+        No retorna nada.
+        
+        ----------
+        lista_personajes : list
+            lista de los jugadores activos en el nivel
+        lista_enemigos : list
+            lista de los enemigos activos en el nivel
+        lista_plataformas : list
+            lista de las plataformas activas en el nivel
+        lista_trampas : list
+            lista de las trampas activas en el nivel
+        lista_balas : list
+            lista de las balas activas en el nivel
+        """
+        
         self.lista_entidades = lista_personajes + lista_enemigos
+        
         if(self.is_shoot):  
             for entidad in self.lista_entidades:
                 if not(self.asset_name == entidad.asset_name or entidad.is_dying):
@@ -67,7 +125,16 @@ class Bullet:
                         self.is_shoot = False
                         break      
     
-    def do_movement(self):
+    def do_movement(self) -> None:
+        """
+        Ajusta la posición de la bala en el eje X en base a los valores actuales de movimiento.
+        
+        No funciona si el tiempo pasado desde la última ejecución del método es inferior al valor de velocidad de desplazamiento.
+        Si el movimiento de la bala la sacaría de los límites de la pantalla, la desactiva.
+        
+        No retorna nada.
+        """
+        
         self.tiempo_transcurrido_move += self.interval_bullet
                 
         if(self.tiempo_transcurrido_move >= self.move_rate_ms):
@@ -83,7 +150,19 @@ class Bullet:
             else:
                 self.is_shoot = False
                 
-    def do_animation (self,delta_ms):
+    def do_animation (self,delta_ms:int) -> None:
+        """
+        Avanza un frame de la animación de la bala hasta llegar al último frame. Si pasa el último frame, vuelve al primero.
+        
+        No funciona si el tiempo pasado desde la última ejecución del método es inferior al valor de velocidad de animación.
+        
+        No retorna nada.
+        
+        ----------
+        delta_ms : int
+            valor de tiempo
+        """
+        
         self.tiempo_transcurrido_anim += delta_ms
         
         if(self.tiempo_transcurrido_anim >= self.frame_rate_ms):
@@ -94,12 +173,42 @@ class Bullet:
             else:
                  self.frame = 0
                 
-    def update (self,delta_ms,lista_personajes,lista_enemigos,lista_plataformas,lista_trampas,lista_balas):
+    def update (self,delta_ms:int,lista_personajes:list,lista_enemigos:list,lista_plataformas:list,lista_trampas:list,lista_balas:list) -> None:
+        """
+        Ejecuta los métodos de animación, movimiento y control de colisiones, actualizando los atributos 
+        de tiempo pasado y elementos activos.
+        
+        ----------
+        delta_ms : int
+            valor de tiempo
+        lista_personajes : list
+            lista de los jugadores activos en el nivel
+        lista_enemigos : list
+            lista de los enemigos activos en el nivel
+        lista_plataformas : list
+            lista de las plataformas activas en el nivel
+        lista_trampas : list
+            lista de las trampas activas en el nivel
+        lista_balas : list
+            lista de las balas activas en el nivel
+        """
+        
         self.shooting (lista_personajes,lista_enemigos,lista_plataformas,lista_trampas,lista_balas)
         self.do_movement()
         self.do_animation(delta_ms)
        
-    def draw (self,screen):        
+    def draw (self,screen): 
+        """
+        Renderiza la bala en la pantalla.
+        
+        ----------
+        screen
+            superficie en la que se renderizan los sprites
+        
+        ----------
+        DEBUG: Renderiza el rectángulo de colisión.
+        """
+        
         if(self.is_shoot):
             if(DEBUG):
                 if(self.collition_enabled):
