e91ff7dbf5d0ab99e6bec301df2a3359b50fdfb1
Santiago Collazo <santiago.collazo7@gmail.com>
2022-12-10 23:50:17 -0300

    Documente sounds.py y trap.py

--- sounds.py
+++ sounds.py
@@ -2,12 +2,44 @@ import pygame
 from constants import *
 
 class Sounds:
-    def __init__ (self,sound_volume=1.0,music_volume=0.25):
+    """Controlador de música y efectos de sonido."""
+    
+    def __init__ (self,sound_volume:float=1.0,music_volume:float=0.25) -> None:
+        """
+        Asigna los valores recibidos a las variables de volúmen de música y 
+        volúmen de efectos de sonido. También establece el volúmen de efectos de
+        sonido por defecto (en caso de que sea necesario desilenciarlos).
+        
+        No retorna nada. 
+              
+        ----------
+        sound_volume : float
+            volúmen de los efectos de sonidos
+        music_volume : float
+            volúmen de música
+        """
+        
         self.sound_volume = sound_volume
         self.sound_volume_standard = sound_volume
         self.music_volume = music_volume
 
-    def set_sound_volume(self,volume):
+    def set_sound_volume(self,volume:float) -> None:
+        """
+        Asigna el valor recibido al volúmen de efectos de sonido.
+        
+        Si el valor es mayor a 0, lo establece como el volúmen a usar 
+        en caso de restaurar los sonidos.
+        
+        No retorna nada. 
+              
+        ----------
+        volume : float
+            volúmen de los efectos de sonidos
+            
+        ----------
+        DEBUG: Imprime el volúmen de sonido asignado.
+        """
+        
         self.sound_volume = volume
         if (self.sound_volume > 0):
             self.sound_volume_standard = self.sound_volume
@@ -15,26 +47,86 @@ class Sounds:
             if(self.sound_volume > 0):
                 print("Sound: {0}".format(self.sound_volume))
         
-    def set_music_volume(self,volume):
+    def set_music_volume(self,volume:float) -> None:
+        """
+        Asigna el valor recibido al volúmen de música.
+        
+        No retorna nada. 
+              
+        ----------
+        volume : float
+            volúmen de música
+            
+        ----------
+        DEBUG: Imprime el volúmen de música.
+        """
+        
         self.music_volume = volume
         if(DEBUG):
             print("Music: {0}".format(self.music_volume))
         
-    def soundtrack (self,music):
+    def soundtrack (self,music:str) -> None:
+        """
+        Reproduce al dirección de archivo recibida como música de nivel.
+        
+        Se reproduce de forma indefinida.
+        
+        No retorna nada.
+              
+        ----------
+        music : str
+            ubicación del archivo de música
+            
+        ----------
+        DEBUG: Imprime el volúmen de música.
+        """
+        
         pygame.mixer.music.load(PATH_RECURSOS + music)
         pygame.mixer.music.set_volume(self.music_volume)
         pygame.mixer.music.play(-1)
 
-    def sound_effect (self,sound):
+    def sound_effect (self,sound:str) -> None:
+        """
+        Reproduce al dirección de archivo recibida como efecto de sonido.
+        
+        Se reproduce una vez,
+        
+        No retorna nada.
+              
+        ----------
+        sound : str
+            ubicación del archivo de efecto de sonido
+        """
+        
         effect = pygame.mixer.Sound(PATH_RECURSOS + sound)
         pygame.mixer.Sound.set_volume(effect,self.sound_volume)
         pygame.mixer.Sound.play(effect)
     
-    def sound_stop(self):
+    def sound_stop(self) -> None:
+        """
+        Detiene la reproducción de todos los efectos de sonido y música.
+
+        No retorna nada.
+        """
+        
         pygame.mixer.stop()
         pygame.mixer.music.stop()
         
-    def music_pause (self,pause=True):
+    def music_pause (self,pause:bool=True) -> None:
+        """
+        Si pause es True, pausa la reproducción de efectos de sonida y 
+        música en el nivel. También silencia cualquier efecto de sonido 
+        nuevo.
+        
+        Si es pause es False, despause la reproducción de efectos de sonida y 
+        música en el nivel. También restora el volúmen de los efectos de sonido 
+        al valor guardado como volúmen por defecto.
+        
+        ----------
+        pause : bool
+            indica si los efectos de sonido y la música deben pausarse o despausarse
+        """
+        
         if (pause):
             pygame.mixer.pause()
             pygame.mixer.music.pause()
--- trap.py
+++ trap.py
@@ -3,10 +3,34 @@ from constants import *
 from auxiliar import Auxiliar
 
 class Trap:
-    def __init__ (self,x,y,w,h,type=0,p_scale=1):
+    """Elemento especial que causa daño a cualquier personaje que colisiona con él."""
+    
+    def __init__ (self,x:int,y:int,w:int,h:int,p_scale:float=1) -> None:
+        """
+        Crea la trampa en base a la carpeta de imagenes indicada y le asigna un 
+        valor de daño (hardcodeado para ser igual a la vida máxima del jugador) y 
+        un efecto de sonido.
+        
+        También genera un rectángulo con las mismas dimensiones que el sprite.
+        
+        No retorna nada.
+              
+        ----------
+        x : int
+            coordenada X en la que se genera la trampa en el nivel
+        y : int
+            coordenada Y en la que se genera la trampa en el nivel
+        w : int
+            ancho del sprite
+        h : int
+            alto del sprite
+        p_scale : float
+            escala del sprite. Por defecto, 1
+        """
+        
         self.asset_name = "Trap"
         self.image_list= Auxiliar.getSurfaceFromSeparateFiles(PATH_RECURSOS + "\\images\\traps\\creepy_forest\\spikes_{:02d}.png",1,step=0,flip=False,scale=p_scale,w=w,h=h)
-        self.image = self.image_list[type]
+        self.image = self.image_list[0]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -17,7 +41,20 @@ class Trap:
         
         self.rect_body_collition = pygame.Rect(self.rect)
     
-    def draw (self,screen):        
+    def draw (self,screen):       
+        """
+        Renderiza el sprite de la plataforma.
+        
+        No retorna nada.
+        
+        ----------
+        screen
+            superficie en la que se renderizan los sprites
+        
+        ----------
+        DEBUG: Renderiza el rectángulo de colisión.
+        """
+         
         if(DEBUG):
             pygame.draw.rect(screen,ORANGE,self.rect_body_collition)
             
